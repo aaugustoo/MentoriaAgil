9d3b6f481b40f597d051fb4e96c3e559cb190080
jvpresideu <152207269+jvpresideu@users.noreply.github.com>
2026-02-12 15:33:37 -0300

    feat(front): implementa tela de cadastro (H1.F1) closes #20

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '', redirectTo: '/cadastro', pathMatch: 'full' }
+];
