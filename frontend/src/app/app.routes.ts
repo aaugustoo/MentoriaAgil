40d2995410720c5c3f2958c2a67b0183311dc674
jvpresideu <152207269+jvpresideu@users.noreply.github.com>
2026-02-12 18:01:45 -0300

    feat: validacao de email e senha (H1.F2) #21

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { CadastroComponent } from './cadastro/cadastro'; 
 
-export const routes: Routes = [
-  { path: 'cadastro', component: CadastroComponent },
-  { path: '', redirectTo: '/cadastro', pathMatch: 'full' }
-];
+export const routes: Routes = [];
